1dc86b437a02aaff9afae4ead9a33a9a54dbc67b
hueptk0711 <hueptka2k23@cvp.vn>
2024-05-15 00:36:14 +0700

    up27

--- myproject/settings.py
+++ myproject/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,7 +126,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'assets'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'accounts'
 
